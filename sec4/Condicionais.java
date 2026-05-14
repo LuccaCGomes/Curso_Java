ab017e510b186e1fdcc9414cf473d1d5fb98925a
Lucca Gomes <lucca.gomes0106@gmail.com>
2026-05-14 13:32:04 -0300

    feat: Add anotações explicando Switch case na aula de Estruturas condicionais

--- sec4/Condicionais.java
+++ sec4/Condicionais.java
@@ -59,9 +59,107 @@ public class Condicionais {
     // =========================================================
 
     /**
-     * switch: Seleciona e executa um bloco de código entre várias 
-     * opções com base no valor de uma expressão.
+     * switch: Permite escolher entre várias opçõescom base no valor 
+     * de uma expressão
+     * 
+     * Case: Define uma opção ou caminho dentro do switch.
+     * 
+     * Break: Utilizado para encerrar a execução de um bloco case. 
+     * Ao contrário do if, que ao entrar em uma das condições, a estrutura
+     * terminava, caso não tenha break (ou outro comando de desvio) no switch case,
+     * após entrar em um dos casos, ele entrará nos casos seguintes.
+     * 
+     * Os valores em case devem ser exclusivos e correspondentes ao tipo
+     * de expressão do switch
+     * 
      */
+    
+        int diaDaSemana = 8;
+
+        switch (diaDaSemana) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-Feira");
+                break;
+            case 3:
+                System.out.println("Terça-Feira");
+                break;
+            case 4:
+                System.out.println("Quarta-Feira");
+                break;
+            case 5:
+                System.out.println("Quinta-Feira");
+                break;
+            case 6:
+                System.out.println("Sexta-Feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;       
+        }
+
+
+    // Switch Case com Default
+
+    /**
+     * Default: O default atua como o else na estrutura if: É executado
+     * quando nenhum dos valores especificados nos case corresponde à
+     * expressão do switch. 
+     * - O seu uso é opcional, mas recomendado para capturar
+     * todos os casos não previstos. 
+     * - Normalmente é colocado no final do switch, mas pode ficar em 
+     * qualquer lugar do bloco. Caso coloque o defalt como último 
+     * (que é a convenção), não é necessário colocar o break. 
+     */
+    
+        int num = 12;
+
+        switch (num) {
+            case 10:
+                System.out.println("O número é 10");
+                break;
+            case 20:
+                System.out.println("O número é 20");
+                break;
+            default:
+                System.out.println("O número não é 10 nem 20");
+                break;
+        }
+
+
+    // Switch sem Break
+
+    /**
+     * - Sem o Break, o switch continua executando os blocos
+     * subsequentes(fall-through), mesmo que o caso correspondente
+     * já tenha sido encontrado, podendo levar à execução de múltiplos
+     * casos e a resultados inexperados.
+     * 
+     * - Por isso é muito recomendado a utilização de break ao final de cada
+     * case
+     * 
+     * - O fall-through, pode ser usado intencionalmente, mas os casos onde
+     * sua utilização se aplica são raros, e depor isso deve ser feito com muito
+     * cuidado e deve ser bem documentado para evitar erros no entendimento do
+     * fluxo do código.
+     */
+
+        int valor = 1;
+
+        switch (valor) {
+            case 1:
+                System.out.println("executou 1");
+            case 2:
+                System.out.println("executou 2");
+            case 3:
+                System.out.println("executou 3");
+            case 4:
+                System.out.println("executou 4");
+            default:
+                System.out.println("executou Default");
+        }
     }
 
 }
