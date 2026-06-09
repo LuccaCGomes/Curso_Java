4ba4239d2fa3ad2ee00ac88a64e35913b5687bda
Lucca Gomes <lucca.gomes0106@gmail.com>
2026-06-09 10:41:55 -0300

    feat: Classe Celular, resolução do exercicio 1 da sec12

---
+++ sec12/Celular.java
@@ -0,0 +1,35 @@
+package sec12;
+
+public class Celular {
+    
+    String marca;
+    String modelo;
+    int bateria = 100;
+    boolean ligado = false;
+
+
+    public void ligarCelular() {
+        if (ligado) {
+            System.out.println("Celular já está ligado!");
+        } else {
+            this.ligado = true;
+            System.out.println("Ligando o celular...");
+        }
+    }
+
+    public void desligarCelular() {
+        if (ligado) {
+            this.ligado = true;
+            System.out.println("Desligando Celular...");
+        } else {
+            
+            System.out.println("Celular já está desligado!");
+        }
+    }
+
+    public void mostrarBateria() {
+        this.bateria -= 7;
+        System.out.println("Bateria: " + bateria + "%");
+    }
+
+}
