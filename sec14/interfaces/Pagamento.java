51442550fde16e15c593f924eee765c9b5506b37
Lucca Gomes <lucca.gomes0106@gmail.com>
2026-07-14 18:02:28 -0300

    feat: add anotação sobre métodos default em interfaces

--- sec14/interfaces/Pagamento.java
+++ sec14/interfaces/Pagamento.java
@@ -3,7 +3,17 @@ package sec14.interfaces;
 interface Pagamento {
     // Método abstrato
     void processarPagamento(double valor);
+    
     // Método default
+    /*
+        São métodos concretos(com implementação) dentro das interfaces
+        
+        Permitem adicionar novas funcionalidades a interfaces existentes sem 
+        quebrar a compatibilidade com  classes que já as implementam
+
+        Com o método default podemos fornecer uma implementação padrão que
+        PODE OU NÃO ser sobrescrita pelas classes que implementarem a interface
+    */
     default void exibirRecibo(double valor) {
         System.out.println("Recibo do Pagamento, com valor de R$" + valor);
     }
